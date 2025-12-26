1073ff9ee92161d27c5a155bcd867bd2a0f6a35d
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-12-26 18:52:02 +0700

    Typed ConfettiContainer

--- app/components/ConfettiContainer.tsx
+++ app/components/ConfettiContainer.tsx
@@ -1,6 +1,11 @@
 import Confetti from "react-confetti";
+import { JSX } from "react";
 
-export default function ConfettiContainer({ isGameWon }) {
+export default function ConfettiContainer({
+  isGameWon,
+}: {
+  isGameWon: boolean;
+}): JSX.Element | null {
   if (!isGameWon) {
     return null;
   } else {
